99023786260ddf78c4a30f4acb969c3ace4d7773
Matilda <mzhang.142857@gmail.com>
2023-10-23 01:38:14 -0400

    nav final temp

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,7 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import Profile from './components/Profile';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -34,7 +35,7 @@ function App() {
           <LoginForm />
         </Route>
         <Route path='/profile' exact={true}>
-          <LoginForm />
+          <Profile />
         </Route>
         <Route path='/sign-up' exact={true}>
           <SignUpForm />
---
+++ react-app/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+const Profile = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  
+  return (
+    <></>
+  );
+};
+
+export default Profile;
