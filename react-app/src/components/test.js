1ea65846204905cf8254b249b96985946fed00a6
Matilda <mzhang.142857@gmail.com>
2023-10-29 15:46:38 -0400

    profile updates

--- react-app/src/components/test.js
+++ react-app/src/components/test.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { MathComponent } from 'mathjax-react'
 import { MathJax } from 'better-react-mathjax';
 
 
