8be8381d8871c2f657df619e194d7541f328a747
Brian <brianshkim25@gmail.com>
2023-10-29 18:41:16 -0700

    post start

--- react-app/src/components/Post.js
+++ react-app/src/components/Post.js
@@ -1,2 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
+const Post = () => {
+    let user = useSelector(state=>state.session.user)
+    let posts = useSelector(state=>state.posts.list)
+}
+
+export default Post
